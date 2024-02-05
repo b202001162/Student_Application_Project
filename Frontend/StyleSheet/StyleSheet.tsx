75b344d94fb6e900005799a5ba5872328dcf7a51
b202001162 <202001162@daiict.ac.in>
2024-02-05 16:15:22 +0530

    Again

---
+++ Frontend/StyleSheet/StyleSheet.tsx
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react';
+import {StyleSheet} from 'react-native';
+
+export const mainStyle = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3f3f3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+  },
+    dContainer: {
+        backgroundColor: '#423f3e',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+    },
+});
